2645bf94867145ae1075f6e122712addfce7b7e5
KsanaSal <oasalivon@gmail.com>
2026-01-25 19:46:33 +0200

    fixed search and input

--- js/search.js
+++ js/search.js
@@ -2,28 +2,13 @@ document.querySelectorAll(".js-search").forEach((search) => {
     const btn = search.querySelector(".js-search-btn");
     const input = search.querySelector(".js-search-input");
 
-    // blur — один раз
-    input.addEventListener("blur", () => {
-        search.classList.remove("is-active");
-    });
-
     btn.addEventListener("click", (e) => {
         e.stopPropagation();
 
-        // закриваємо всі інші пошуки
-        document.querySelectorAll(".js-search").forEach((el) => {
-            if (el !== search) el.classList.remove("is-active");
-        });
-
         search.classList.toggle("is-active");
-
-        if (search.classList.contains("is-active")) {
-            input.focus();
-        }
     });
 });
 
-// один глобальний клік
 document.addEventListener("click", () => {
     document
         .querySelectorAll(".js-search")
